e3bf958db25b24fdd87a3855a03edb620e09e19a
emartin <emartin@transperfect.com>
2014-09-23 16:17:12 +0100

    Reddit oAuth working. Only issue is window mangement (focusing/closing etc).

--- src/app/auth/callback.js
+++ src/app/auth/callback.js
@@ -6,8 +6,47 @@
 		results = regex.exec(location.search);
 		return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 	}
-	console.log('State: ' + App.Auth.state);
-	console.log('Returned State: '+ getParameterByName('state'));
-	console.log('Error: '+ getParameterByName('error'));
-	console.log('Code: '+ getParameterByName('code'));
+	var error = getParameterByName('error');
+	if(error) {
+		if(error === 'access_denied') {
+			console.log('User denied access');
+			window.opener.focus();
+			window.close();
+			return;
+		}
+		return alert(error);
+	}
+
+	if(getParameterByName('state') !== App.Auth.state) {
+		return alert('Error!');
+	}
+
+	var code = getParameterByName('code');
+
+	window.opener.$.ajax({
+		type: "POST",
+		url: 'https://ssl.reddit.com/api/v1/access_token',
+		data: {
+			'grant_type': 'authorization_code',
+			'code': code,
+			'redirect_uri': App.Auth.Config.callback_url
+		},
+		beforeSend: function (xhr) {
+			xhr.setRequestHeader ("Authorization", "Basic " + btoa(App.Auth.Config.client_id + ":"+ App.Auth.Config.secret_key)); 
+		},
+		success: function(data, textStatus, jqXHR) {
+			console.log(data);
+			if(data.access_token) {
+				localStorage.token = data.access_token;
+			};
+			window.opener.focus();
+			window.close();
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(textStatus);
+			alert(errorThrown);
+			return;
+		}
+	});
+
 })(window.opener.App);
